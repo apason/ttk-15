0cddcf60077243727a8ae6fbd4f23dcb9c87f7b4
Hiski Ruhanen <hiski.ruhanen@helsinki.fi>
2015-03-24 21:40:17 +0200

    reading a file now works with comments, empty lines and indentations stripped away

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -6,14 +6,18 @@ int countLines(FILE*);
 
 int main (int argc, char* argv[]) {
 
+	if (argc < 2) return -1;
+
 	// we take the first argument to be name of a .ttk91 file
-	char* file;
-	if (argc > 1) 	file = argv[1];
-	else		return -1;
+	char* filename = argv[1];
+
+	printf("file is : %s\n",filename);
 
 	// open file and count the lines but not comments
-	FILE* fh = fopen(file,"r");
+	FILE* fh = fopen(filename,"r");
 	int lines = countLines(fh);
+	printf("lines = %d\n",lines);
+	if (lines == -1) return -1;
 	rewind(fh);
 	
 	// reserve space and read the code lines from the file
@@ -23,23 +27,35 @@ int main (int argc, char* argv[]) {
 			printf("Error reading the source file!\n");
 			return -1;
 		}
-		input[i] = (char*) malloc(256 * sizeof(char*));
-		int ch = fgetc(fh), count = 0;
+		int ch = fgetc(fh);
+		// skip whitespaces
 		while (ch == ' ' || ch == '\t') ch = fgetc(fh);
-		if (ch == ';') {
+		// Skip comment lines
+		while (ch == ';' || ch == '\n') {
+			if (ch == '\n') {
+				ch = fgetc(fh);
+				continue;
+			}
 			while (fgetc(fh) != '\n');
 			ch = fgetc(fh);
 			while (ch == ' ' || ch == '\t') ch = fgetc(fh);
 		}
-		while (ch != '\n') {
+		// reserve space for the next line
+		input[i] = (char*) malloc(256 * sizeof(char));
+		int count = 0;
+		while (ch != EOF && ch != '\n') {
 			if (ch == ';') {
 				while (fgetc(fh) != '\n');
-				continue;
+				break;
 			}
-			*input[count++] = ch;
+			input[i][count++] = ch;
+			ch = fgetc(fh);
 		}
-		*input[count] = 0;
+		input[i][count] = 0;
+		printf("Line %d: %s\n",i+1,input[i]);
+	
 	}
+	fclose(fh);
 
 	// free space for the file array
 	for (int i = 0; i < lines; ++i) {
@@ -49,16 +65,33 @@ int main (int argc, char* argv[]) {
 	return 0;
 }
 
+int nextLine(FILE* fh) {
+	int ch = fgetc(fh);
+	// Strip whitespaces and tabs from beginning
+	while (ch != EOF && ch == ' ' || ch == '\t') ch = fgetc(fh);
+	// If this line is a comment, let's skip it
+	if (ch == ';') {
+		while ((ch = fgetc(fh)) != EOF && ch != '\n');
+		return 0;
+	}
+	// If this line is empty, skip it too
+	if (ch == '\n') {
+		fgetc(fh);
+		return 0;
+	}
+	// If we have reached end of file, there is no line
+	if (ch == EOF) return 0;
+	// Valid line so seek through and add 1 to line count
+	while (ch != EOF && ch != '\n') ch = fgetc(fh);
+	return 1;
+}
+
 int countLines (FILE* fh) {
 	if (fh == NULL) return -1;
 	int lines = 0;
 	int ch = 0;	
 	while (!feof(fh)) {
-		ch = fgetc(fh);
-		while (ch == ' ' || ch == '\t') ch = fgetc(fh);
-		if (ch == ';') continue;
-		while (ch != '\n') fgetc(fh);
-		++lines;
+		lines += nextLine(fh);
 	}
 	return lines;
 }
