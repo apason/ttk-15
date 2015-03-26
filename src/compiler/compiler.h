17d9c1d611a1458c17ab5839ed7eabb698c45957
apa <arttu.kilpinen@helsinki.fi>
2015-03-26 03:03:09 +0200

    pull

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -1,14 +1,21 @@
+#ifndef COMPILER
+#define COMPILER
+
 typedef struct {
 	char* name;
 	int lines;
 	char** array;
 } code_file;
+
 typedef struct label_list {
 	struct label_list* next;
 	char label[128];
 	int address;
 	int size;
 } label_list;
+
 int countSize(char**, int, label_list*);
 int countLines(FILE*);
 char** readCode(FILE*,int);
+
+#endif 
