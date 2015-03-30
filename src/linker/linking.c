aff47557d6e8e7b5546d1fbb9262a1c9cd0af7c3
apa <arttu.kilpinen@helsinki.fi>
2015-03-31 00:26:12 +0300

    debug

--- src/linker/linking.c
+++ src/linker/linking.c
@@ -114,8 +114,6 @@ static int16_t findLabelValue(module **modules, int n, char *label){
     exit(-1);
   }
 
-  printf("modules: %d\n", n);
-  
   for(i = 0; i < n; i++)
     for(s = modules[i]->symbols; s; s = s->next)
       if(!strncmp(s->label, label, LABELLENGTH) && s->value >= 0)
