b6fb80a9b2a35dcb1db8e287d4d9452ba8fe1a71
apa <arttu.kilpinen@helsinki.fi>
2015-04-16 21:46:12 +0300

    v0.2

--- src/machine/main.c
+++ src/machine/main.c
@@ -6,28 +6,31 @@
 #include "machine.h"
 
 int main(int argc,char *argv[]){
-  options *opts = getOptions(argc, argv);
-  machine *m    = newMachine(opts->memsize);
+    options *opts = getOptions(argc, argv);
+    machine *m    = newMachine(opts->memsize);
 
-  m->cu->pc = 0;
+    m->cu->pc = 0;
 
-  if(opts == NULL){
-    freeMachine(m);
-    return -1;
-  }
+    if(opts == NULL){
+	freeMachine(m);
+	return -1;
+    }
 
-  if(opts->mode == TTK91)
-    m->regs[6] = (MYTYPE) loadFile91(m->mem, opts->file);
-  else if(opts->mode == TTK15)
-    m->regs[6] = (MYTYPE) loadFile(m->mem, opts->file);
-       
-  m->regs[7] = m->regs[6];
+    if(opts->mode == TTK91)
+	m->regs[6] = (MYTYPE) loadFile91(m->mem, opts->file, opts->memsize);
+    else if(opts->mode == TTK15)
+	m->regs[6] = (MYTYPE) loadFile(m->mem, opts->file, opts->memsize);
+
+    //otherwise error occured
+    if(m->regs[6] > 0){
+	m->regs[7] = m->regs[6];
   
-  startMachine(m, opts->debug);
-  freeMachine(m);
-  freeOptions(opts);
+	startMachine(m, opts->debug);
+    }
+    freeMachine(m);
+    freeOptions(opts);
 
-  printf("\n");
+    printf("\n");
 
-  return 0;
+    return 0;
 }
