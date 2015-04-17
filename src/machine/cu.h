a79e5b75dc58348e1c589a5d4f5c1d47dd97ffa3
apa <arttu.kilpinen@helsinki.fi>
2015-04-17 04:05:09 +0300

    calculateSecondOperand makroksi

---
+++ src/machine/cu.h
@@ -0,0 +1,39 @@
+#include <ttk-15.h>
+
+#ifndef int_i_macro
+#define int_i_macro
+static int    _i;
+#endif
+
+#ifndef MYTYPE_tmp_macro
+#define MYTYPE_tmp_macro
+static MYTYPE _tmp;
+#endif
+
+#ifndef MYTYPE_result_macro
+#define MYTYPE_result_macro
+static MYTYPE _result;
+#endif
+
+/*
+ * ri=0 means no index register used 
+ * so this function first saves r0, 
+ * puts 0 to it and recovers when done
+ */
+
+#define calculateSecondOperand(M, MODE, RI, ADDR) \
+    _result = (MYTYPE) ADDR;			  \
+    _tmp = M->regs[0];                            \  //save r0
+    M->regs[0] = 0;                               \  //set r0 to 0
+    _result += M->regs[ri];                       \
+                                                  \
+   \ //fetch as many times as needed               
+    for(_i = 0; _i < MODE; _i++){                 \
+	mmuGetData(M->mmu, M->mem, _result);      \
+	_result = M->mmu->mbr;                    \
+    }                                             \ 
+			         		  \
+    M->regs[0] = _tmp;                            \  //restore r0
+
+    
+
