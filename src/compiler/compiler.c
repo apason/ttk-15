fea12db8573935bb875abc069026ba6fa3f6a6c5
Hiski Ruhanen <hiski.ruhanen@helsinki.fi>
2015-09-06 01:06:15 +0300

    now debug mode also prints usage table

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -32,7 +32,7 @@ int main (int argc, char* argv[]) {
         // and set the code_file struct to hold the filehandle and output name
         code_file codeFile;
         codeFile.name = argv[n];
-        codeFile.mode = TTK91 & debug << 2;
+        codeFile.mode = TTK91 | (debug << 2);
         codeFile.fh_out = output++;
 
         // read the file into the struct
@@ -43,9 +43,9 @@ int main (int argc, char* argv[]) {
         char* suffix = strrchr(argv[n], '.');
         if (opts->mode == UNDEFINED && suffix != NULL) {
             if (!strncmp(suffix,".k15",5))
-            codeFile.mode = TTK15 & debug << 2;
+            codeFile.mode = TTK15 | (debug << 2);
         } else
-            codeFile.mode = opts->mode & debug << 2;
+            codeFile.mode = opts->mode | (debug << 2);
 
 
         // calculate code size and create the symbol table
@@ -75,6 +75,12 @@ int main (int argc, char* argv[]) {
             while (temp != NULL) {
                 printf("%s : %0x : %d : %d : %s\n",temp->label, (uint16_t)temp->address, temp->size, temp->value, mode[temp->mode]);
                 temp = temp->next;
+            }
+            printf("Label locations:\n");
+            usage_list* utemp = codeFile.usageList;
+            while (utemp != NULL) {
+                printf("%0x : %s \n",utemp->value, utemp->label);
+                utemp = utemp->next;
             }
         }
         // free the space reserved for dynamic data in codeFile
